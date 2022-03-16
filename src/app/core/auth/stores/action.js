00917a86678bfbd6dc73bdfd484aa966304d703c
quang <asdquang111@gmail.com>
2022-03-16 19:57:56 +0700

    Deploy 16/3

--- src/app/core/auth/stores/action.js
+++ src/app/core/auth/stores/action.js
@@ -1,13 +1,13 @@
-import * as types from "./types";
-import { AuthService } from "../../services/auth.service";
-import { setModal } from "../../../stores/modal/action";
+import * as types from './types';
+import { AuthService } from '../../services/auth.service';
+import { setModal } from '../../../stores/modal/action';
 
 const http = new AuthService();
 
 export const login = (dataLogin, navigate) => async (dispatch) => {
   try {
     let response;
-    if (dataLogin.email === "admin@gmail.com") {
+    if (dataLogin.email === 'admin@gmail.com') {
       const newData = {
         username: dataLogin.email,
         password: dataLogin.password,
@@ -16,18 +16,28 @@ export const login = (dataLogin, navigate) => async (dispatch) => {
     } else {
       response = await http.signIn(dataLogin);
     }
-    dispatch(
-      setModal({
-        key: "snapback",
-        title: "",
-        content: "Đăng nhập thành công",
-      })
-    );
-    dispatch({
-      type: types.LOGIN_SUCCESS,
-      payload: response,
-    });
-    navigate('/');
+    if (response?.role === 'customer') {
+      dispatch(
+        setModal({
+          key: 'error',
+          title: 'Error',
+          content: 'Tài khoản hoặc mật khẩu không chính xác',
+        })
+      );
+    } else {
+      dispatch(
+        setModal({
+          key: 'snapback',
+          title: '',
+          content: 'Đăng nhập thành công',
+        })
+      );
+      dispatch({
+        type: types.LOGIN_SUCCESS,
+        payload: response,
+      });
+      navigate('/');
+    }
   } catch (error) {
     dispatch({
       type: types.LOGIN_FAIL,
--- src/app/pages/product/components/ListComment.jsx
+++ src/app/pages/product/components/ListComment.jsx
@@ -1,23 +1,26 @@
+import { convertDate } from '../../../shared/helpers/utils/convertDate';
+
 const ListComment = ({ data }) => {
   return (
     <>
       {data && data.length ? (
-        <div className="comment-product">
-          <ul className="list-comment">
+        <div className='comment-product'>
+          <ul className='list-comment'>
             {data.map((item, index) => (
-              <li className="item-comment" key={index}>
-                <p className="comment-user">{item.customer[0]?.name}</p>
+              <li className='item-comment' key={index}>
+                <p className='comment-user'>{item.customer[0]?.name}</p>
                 <div>
                   {[...Array(item.rating)].map((item1, index1) => (
                     <span
-                      className="material-icons-outlined icon-star"
+                      className='material-icons-outlined icon-star'
                       key={index1}
                     >
                       star
                     </span>
                   ))}
                 </div>
-                <p className="comment">{item.comment}</p>
+                <p className='comment'>{item.comment}</p>
+                <p className='time'>{convertDate(item.createdAt)}</p>
               </li>
             ))}
           </ul>
